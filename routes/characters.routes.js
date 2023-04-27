41cb0cc1bfeac2c6588fc124603d76c9e24dbf44
ahmed awad <ahmeedsauce7@gmail.com>
2023-04-27 16:32:34 +0200

    details trying

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -4,3 +4,35 @@ const bcryptjs = require("bcryptjs");
 const saltRounds = 13;
 const isLoggedIn = require('../middleware/route-guard')
 const isLoggedOut = require('../middleware/route-guard')
+const character = require("../models/character.model");
+
+router.get('/', async (req, res, next) => {
+    try {
+        const characters = await character.find({player:req.session.user})
+        res.render("characterview", {characters});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get("/", (req, res, next) => {
+    res.render("main");
+  });
+// add is loggedin
+  router.get("/:Id/", async (req, res) => {
+    try {
+      const characters = await character.findById(req.params.Id);
+      res.render("characterDetails", { characters });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+// add is loggedin
+  router.get("/:Id/details", async (req, res) => {
+    try {
+      const characters = await character.findById(req.params.Id);
+      res.render("characterDetails", { characters });
+    } catch (error) {
+      console.log(error);
+    }
+  });

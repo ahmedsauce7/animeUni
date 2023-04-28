303411e933e0bc0bb585bb08a04875f434c89a23
Denis Röder <d-roeder@hotmail.de>
2023-04-28 15:18:10 +0200

    weapons.js, shop.model.js,shop.routes.js(Ahmed)

---
+++ views/weapons.js
@@ -0,0 +1,56 @@
+const shop = require('../models/shop.model');
+require("../db");
+
+const weapons = [
+{
+    name: "Devil's Fruit",
+    price: 250,
+    image: '/images/dfruit.png'
+},
+{
+    name: "Dance of The Fire God",
+    price: 250,
+    image: '/images/fireGod.jpg'
+},
+{
+    name: "Susanoo",
+    price: 250,
+    image: '/images/susanoo.png'
+},
+{
+    name: "Tsukuyomi",
+    price: 250,
+    image: '/images/Tsukuyomi.png'
+},
+{
+    name: "Ryujin Jakka",
+    price: 250,
+    image: '/images/Jakka.jpg'
+},
+{
+    name: "Hakai",
+    price: 250,
+    image: '/images/hakai.png'
+},
+{
+    name: "Serious Punch",
+    price: 250,
+    image: '/images/punch.png'
+},
+{
+    name: "Full Counter",
+    price: 250,
+    image: '/images/Counter.png'
+},
+{
+    name: "Spirit Bomb",
+    price: 250,
+    image: '/images/Spirit.png'
+},
+{
+    name: "Fourth Gear",
+    price: 250,
+    image: '/images/Gear.png'
+},
+];
+

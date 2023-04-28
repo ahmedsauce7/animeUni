11cc03007124b7510e26e23872f8c46d112d387e
ahmed awad <ahmeedsauce7@gmail.com>
2023-04-28 20:16:07 +0300

    FINALLY DETAILS ARE HEREEE

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -6,47 +6,65 @@ const {isLoggedIn} = require('../middleware/route-guard')
 const {isLoggedOut} = require('../middleware/route-guard')
 const character = require("../models/character.model");
 
-router.get('/', (req, res, next) => {
-    res.render('characterDetails')
+router.get('/', isLoggedIn, async (req, res, next) => {
+  try { 
+    const user = req.session.player;
+    const currentChar = await character.find({player: user})
+    res.render('characterDetails', {currentChar})
+  } catch (error) {
+    console.log(error)
+  }
 });
-
-// router.post("/create", isLoggedIn, async (req, res) => {
-//   try {
-//     let imgUrl;
-//     if (req.body.universe === "Human" && req.body.gender === "Male") {
-//       imgUrl = "/images/charImages/HumanM4.webp";
-//     } else if (req.body.universe === "Human" && req.body.gender === "Female") {
-//       imgUrl = "/images/charImages/HumanF.png";}
-//     } catch(error){
-//       console.log(error)
-//     }
-//   });
    
 
-router.post('/create', async (req, res, next) => {
-    try { 
-        const characters = await character.create(req.body)
-        res.redirect(`/characters/${characters.id}`)
-    } catch(error) {
-        console.log (error)
-    }
-})
+// router.post('/create', async (req, res, next) => {
+//     try { 
+//         const characters = await character.create(req.body)
+//         res.redirect(`/characters/${characters.id}`)
+//     } catch(error) {
+//         console.log (error)
+//     }
+// })
 
-router.get ('/:id', isLoggedIn, async (req, res, next) => {
+router.get ('/:id/', isLoggedIn, async (req, res, next) => {
     try {
 const newCharacter = await character.findOne({ id: req.params.id })
+console.log(newCharacter)
 res.render('characterDetails', {newCharacter})
     } catch (error) {
         console.log(error)
     }
 })
 
+router.post("/create", isLoggedIn, async (req, res) => {
+  try {
+    let charImg;
+    if (req.body.universe === "Human" && req.body.gender === "Male") {
+      charImg = "/images/charImages/HumanM4.webp";
+    } else { (req.body.universe === "Human" && req.body.gender === "Female") 
+      charImg = "/images/charImages/HumanF.png";}
+      const charCreation = await character.create({
+      name: req.body.name,
+      universe: req.body.universe,
+      gender: req.body.gender,
+      nickname: req.body.nickname,
+      weakness: req.body.weakness,
+      weapon: req.body.weapon,
+      image: charImg,
+      player: req.session.player
+    });
+    res.redirect(`/characters/${charCreation.id}/details`)
+  } catch (error) {
+      console.log(error)
+    }
+  });
+
 // 1. Update, -> Delete
 
 router.get("/:id/details",isLoggedIn,async (req,res, next)=> {
     try {
-        const character = await character.findById(req.params.id);
-        res.render("characterDetails",{character});
+        const findChar = await character.findById(req.params.id);
+        res.render("characterDetails",{findChar});
     } catch (error) {
         console.log(error);
     }

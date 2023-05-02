6a6939693f3d1a414f314f0c37c1b5f22548712f
ahmed awad <ahmeedsauce7@gmail.com>
2023-05-02 13:17:53 +0300

    trying to get correct mvp

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -5,15 +5,4 @@ const saltRounds = 13;
 const {isLoggedIn} = require('../middleware/route-guard')
 const {isLoggedOut} = require('../middleware/route-guard')
 const uploader = require('../middleware/cloudinary.config.js');
-const Character = require("../models/character.model");
 
-  
-  router.get("/items", isLoggedIn, async (req, res) => {
-    try {
-      const currentUser = req.session.user;
-      const shopItems = await Shop.find();
-      res.render("items", { shopItems, currentUser });
-    } catch (error) {
-      console.log(error);
-    }
-  });
